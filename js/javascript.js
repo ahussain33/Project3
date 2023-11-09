4f854efb6bfc6f18523c9cead4380389fe74c037
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-11-09 12:05:24 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,13 @@
 function init(){
-  
+  var buttonForward = document.getElementById("nextbutton1")
+  var buttonBack = document.getElementById("nextbutton2")
+  buttonForward.addEventListener("click", forward)
+  buttonBack.addEventListener("click", back)
 }
-
+function forward(){
+  document.getByElementId("malePeacockOne")
+}
+function back(){
+  document.getByElementId("malePeacockTwo")
+}
 window.addEventListener('load', init);
