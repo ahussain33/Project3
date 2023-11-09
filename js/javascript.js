e19533c29f9d1dfebdb42e22864e4ca87379a4d9
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-11-09 15:37:25 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,17 @@
 function init(){
-  var change = document.getByElementId("malePeacockOne")
-  var buttonForward = document.getElementById("nextbutton1")
-  var buttonBack = document.getElementById("nextbutton2")
-  buttonForward.addEventListener("click", forward)
-  buttonBack.addEventListener("click", back)
+  var change = document.getByElementId("malePeacockOne");
+  var buttonForward = document.getElementById("nextbutton1");
+  var buttonBack = document.getElementById("nextbutton2");
+  buttonForward.addEventListener("click", forward);
+  buttonBack.addEventListener("click", back);
 
   function forward(){
-    change.src = ("media/malepeacock2.jpg")
+    change.src = ("media/malepeacock2.jpg");
   }
   
   function back(){
-   change.src = ("media/malepeacock.jpg")
+   change.src = ("media/malepeacock.jpg");
   }
 }
 window.addEventListener('load', init);
+google.maps.event.addDomListener(window, 'load', init);
