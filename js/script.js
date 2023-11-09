cf9419567be6831e303be66b54e30d4a329c2e62
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-11-09 16:34:05 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,12 @@
-function init(){
-  var change = document.getByElementId("malePeacockOne");
-  var buttonForward = document.getElementById("nextbutton1");
-  var buttonBack = document.getElementById("nextbutton2");
-  buttonForward.addEventListener("click", forward);
-  buttonBack.addEventListener("click", back);
-
-  function forward(){
-    change.src = ("media/malepeacock2.jpg");
-  }
-  
-  function back(){
-   change.src = ("media/malepeacock.jpg");
-  }
-}
-// Initialize and add the map
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "9981a68191e6f096",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
 }
 
 initMap();
-window.addEventListener('load', init);
