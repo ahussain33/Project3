46945aced321e242bf492db0cd76a92bceb36ace
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-11-10 17:05:30 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
 function initMap() {
   const myLatLng = {lat: 41.83155375445465, lng: -87.62724193181992};
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
+    zoom: 18,
     center: myLatLng,
   });
 
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "Hello World!",
-  });
-  
   const contentString =
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<h1 id="firstHeading" class="firstHeading">IIT</h1>' +
     '<div id="bodyContent">' +
-    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "<p><b>IIT</b>, also referred to as <b>Illinois Tech</b>, is a large " +
     "sandstone rock formation in the southern part of the " +
     "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
     "south west of the nearest large town, Alice Springs; 450&#160;km " +
@@ -34,18 +28,21 @@ function initMap() {
     "</div>";
   const infowindow = new google.maps.InfoWindow({
     content: contentString,
-    ariaLabel: "Uluru",
+    ariaLabel: "IIT",
   });
+  
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: myLatLng,
     map,
-    title: "Uluru (Ayers Rock)",
-  });
+    title: "Hello World!",
+  }); 
 
   marker.addListener("click", () => {
     infowindow.open({
       anchor: marker,
       map,
-}
-
+    });
+  });
+  
+ }
 window.initMap = initMap;
