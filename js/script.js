b09f72f6d44182ef5efd05f8771088ca43fdae8c
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-11-09 18:50:07 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,14 +12,13 @@ function init(){
     change.src = ("media/malepeacock.jpg");
  }
 }
- function initMap() {
+function initMap() {
     var dumbo = {lat: 40.700802, lng:73.987602};
     var mapOptions = {
         center: dumbo,
         zoom: 10
     };
         var googlemap = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
 }
 window.addEventListener('load', init);
 
