f59082f9ad4a97b25c045f1d5973ffb149c0421f
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-11-09 19:10:15 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
-function init(){
-  var change = document.getByElementId("malePeacockOne");
-  var buttonForward = document.getElementById("nextbutton1");
-  var buttonBack = document.getElementById("nextbutton2");
-  buttonForward.addEventListener("click", forward);
-  buttonBack.addEventListener("click", back);
-
- function forward(){
-    change.src = ("media/malepeacock2.jpg");
- }
- function back(){
-    change.src = ("media/malepeacock.jpg");
- }
-}
+var map
 function initMap() {
     var dumbo = {lat: 40.700802, lng:73.987602};
-    var mapOptions = {
+        var map = new google.maps.Map(document.getElementById("map"), {
         center: dumbo,
-        zoom: 10
-    };
-        var googlemap = new google.maps.Map(document.getElementById("map"), mapOptions);
+        zoom: 15
+  });
 }
-window.addEventListener('load', init);
 
