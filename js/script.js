8c35c0bc94bb4cebef9ac7c67b9aacd9cd7f5fcd
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-11-09 20:10:06 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var map;
+let map;
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
+        var map = new google.maps.Map(document.getElementById("map"), {
             center: {lat: 40.700802, lng:73.987602}, 
-            zoom: 8
+            zoom: 8,
         });
 }
 
