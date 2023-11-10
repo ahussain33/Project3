0bcea55816fdc4ee6e3631759acd41ff1df37060
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-11-09 19:43:31 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var map;
 function initMap() {
-    var dumbo = {lat: 40.700802, lng:73.987602};
-        var map = new google.maps.Map(document.getElementById("map"), {center: dumbo, zoom: 15});
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 40.700802, lng:73.987602}, 
+            zoom: 8
+        });
 }
 
