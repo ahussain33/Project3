ba07fb6d531ff0a96bf5e5d21d537aa1bae8b690
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-11-10 16:50:43 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-let map;
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById("map"), {
-            center: {lat: 41.83155375445465, lng: -87.62724193181992}, 
-            zoom: 12
-        });
-   }
+function initMap() {
+  const myLatLng = {lat: 41.83155375445465, lng: -87.62724193181992};
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatLng,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+}
+
+window.initMap = initMap;
